b65093d7c85431964a6d34c31257f0e76ada9fbd
kimyh5392 <mk.kimyh@gmail.com>
2018-01-08 18:30:57 +0900

    모바일 헤더 gnb 및 mypage 퍼블리싱 완료.

--- mobile/inc/header.php
+++ mobile/inc/header.php
@@ -15,12 +15,257 @@
             <img src="/mobile/assets/images/btn_mypage.jpg" alt="버튼 아이콘">
         </button>
     </div>
-    <nav id="gnb">
+    <div id="gnb">
         <ul>
             <li><a href="#">e매거진</a></li>
             <li><a href="#">체험이벤트</a></li>
             <li><a href="#">커뮤니티</a></li>
             <li><a href="#">고객센터</a></li>
         </ul>
+    </div>
+
+    <!-- nav menu -->
+    <nav id="gnb-nav">
+        <div class="nav-backdrop"></div>
+        <div class="nav-container">
+            <div class="nav-header">
+                <button type="button" class="btn btn-close">
+                    <img src="/mobile/assets/images/nav_btn_close.png" alt="네비 닫기">
+                </button>
+                <div class="global-search">
+                    <form action="#">
+                        <div class="form-group">
+                            <input type="text" id="global-search" class="form-control" placeholder="Search">
+                            <label for="global-search" class="sr-only">검색</label>
+                        </div>
+                        <div class="search-btn">
+                            <button type="submit" class="btn btn-default">
+                                <i class="icon-search">
+                                    <span class="sr-only">검색버튼</span>
+                                </i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="nav-body">
+                <div class="quick-menu">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/mobile/assets/images/icon_samplepack.gif" alt="샘플팩 신청">
+                                </figure>
+                                <p>샘플팩 신청</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/mobile/assets/images/icon_prizewinner.gif" alt="당첨자 발표">
+                                </figure>
+                                <p>당첨자 발표</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/mobile/assets/images/icon_customer.gif" alt="고객센터">
+                                </figure>
+                                <p>고객센터</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="gnb-menu">
+                    <ul>
+                        <li>
+                            <a href="#">e매거진</a>
+                            <ul>
+                                <li><a href="#">e-book</a></li>
+                                <li><a href="#">임신/출산</a></li>
+                                <li><a href="#">성장/건강</a></li>
+                                <li><a href="#">놀이/교육</a></li>
+                                <li><a href="#">리빙/헬스</a></li>
+                                <li><a href="#">앙쥬스토리</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">e매거진</a>
+                            <ul>
+                                <li><a href="#">e-book</a></li>
+                                <li><a href="#">임신/출산</a></li>
+                                <li><a href="#">성장/건강</a></li>
+                                <li><a href="#">놀이/교육</a></li>
+                                <li><a href="#">리빙/헬스</a></li>
+                                <li><a href="#">앙쥬스토리</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">e매거진</a>
+                            <ul>
+                                <li><a href="#">e-book</a></li>
+                                <li><a href="#">임신/출산</a></li>
+                                <li><a href="#">성장/건강</a></li>
+                                <li><a href="#">놀이/교육</a></li>
+                                <li><a href="#">리빙/헬스</a></li>
+                                <li><a href="#">앙쥬스토리</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">e매거진</a>
+                            <ul>
+                                <li><a href="#">e-book</a></li>
+                                <li><a href="#">임신/출산</a></li>
+                                <li><a href="#">성장/건강</a></li>
+                                <li><a href="#">놀이/교육</a></li>
+                                <li><a href="#">리빙/헬스</a></li>
+                                <li><a href="#">앙쥬스토리</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="gnb-banner">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/mobile/assets/images/img_gnb_banner01.jpg" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/mobile/assets/images/img_gnb_banner02.jpg" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <figure>
+                                    <img src="/mobile/assets/images/img_gnb_banner03.jpg" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="sns-list">
+                    <ul>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/mobile/assets/images/icon_naver_post.png" alt="네이버 포스트">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/mobile/assets/images/icon_naver_blog.png" alt="네이버 블로그">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/mobile/assets/images/icon_kakao.png" alt="카카오스토리">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/mobile/assets/images/icon_facebook.png" alt="페이스북">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/mobile/assets/images/icon_insta.png" alt="인스타그램">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/mobile/assets/images/icon_youtube.png" alt="유튜브">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- mypage -->
+    <nav id="mypage-nav">
+        <div class="nav-backdrop"></div>
+        <div class="nav-container">
+            <div class="nav-header">
+                <button type="button" class="btn btn-close">
+                    <img src="/mobile/assets/images/nav_btn_close.png" alt="네비 닫기">
+                </button>
+                <div class="user-info">
+                    <h2>
+                        무브먼트 <small>님</small>
+                        <a href="#" class="logout-btn" role="button">
+                            <i class="icon-off">
+                                <span class="sr-only">로그아웃하기</span>
+                            </i>
+                        </a>
+                    </h2>
+                    <div class="user-profile">
+                        <div class="profile">
+                            <figure>
+                                <img src="/mobile/assets/images/user_profile_none_img.jpg" alt="">
+                            </figure>
+                        </div>
+                        <div class="information">
+                            <ul class="mileage">
+                                <li><b>M</b> 123,456</li>
+                                <li><b>C</b> 123</li>
+                            </ul>
+                            <dl class="d-day">
+                                <dt>출산예정</dt>
+                                <dd><b>D-150</b>일</dd>
+                                <dt>결혼기념일</dt>
+                                <dd><b>D-365</b>일</dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ange-event">
+                <a href="#">
+                    <h3>앙쥬네이밍 <small>우리아이 이름짓기</small></h3>
+                </a>
+            </div>
+            <div class="nav-body">
+                <div class="interest-keyword">
+                    <a href="#" class="title">관심키워드</a>
+                    <ul>
+                        <li><a href="#">주차별정보</a></li>
+                        <li><a href="#">임신준비/태교</a></li>
+                        <li><a href="#">출산준비/용품</a></li>
+                        <li><a href="#">DIY/놀이법</a></li>
+                        <li><a href="#">이유식/푸드</a></li>
+                    </ul>
+                </div>
+                <div class="my-interest">
+                    <a href="#" class="title">나의 관심글</a>
+                    <ul class="board-list">
+                        <li><a href="#">[임신] 심한 입덧 가라앉히는 방법 없이 가라앉히는 방법 없이</a></li>
+                        <li><a href="#">[출산] 산후조리할 때 어떤 음식을 챙때 어떤 음식을 챙</a></li>
+                        <li><a href="#">[임신] 어떤 산부인과를 다녀야 할까다녀야 할까</a></li>
+                    </ul>
+                </div>
+                <div class="experience-event">
+                    <a href="#" class="title">나의 관심글</a>
+                    <ul class="board-list">
+                        <li><a href="#">[서평단]썬비의 육아 가계부썬비의 육아 가계부 육아 가계부</a></li>
+                        <li><a href="#">[비세이프] 안전 최고평가,안전 최고평가,안전 최고평가, 최고평가,</a></li>
+                        <li><a href="#">[서평단]내 아이를 위한 그내 아이를 위한 그그내 아이를 위한 그</a></li>
+                    </ul>
+                </div>
+                <div class="community">
+                    <a href="#" class="title">커뮤니</a>
+                    <ul class="board-list">
+                        <li><a href="#">[서평단]썬비의 육아 가계부썬비의 육아 가계부 육아 가계부</a></li>
+                        <li><a href="#">[비세이프] 안전 최고평가,안전 최고평가,안전 최고평가, 최고평가,</a></li>
+                        <li><a href="#">[서평단]내 아이를 위한 그내 아이를 위한 그그내 아이를 위한 그</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </nav>
 </header>
