317bbeb85c0d341f7da1ee9e3d3d9388f219bd94
Joahra <mk_ahra0522@gmail.com>
2018-01-08 19:29:21 +0900

    모바일 마이페이지 > 마일리지,주문현황 완료

---
+++ mobile/mypage/order_info.php
@@ -0,0 +1,219 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/mobile/inc/dochead.php"); ?>
+<link href="/mobile/assets/css/sub.css" rel="stylesheet">
+<link href="/mobile/assets/css/mypage.css" rel="stylesheet">
+</head>
+<body class="sub mypage"><!-- 폴더별 클래스명 추가 -->
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/mobile/inc/header.php"); ?>
+    <!-- pgae-header -->
+    <div class="page-header">
+        <!-- 폴더별 타이틀 -->
+        <h2>주문현황</h2>
+        <div class="btn-controls">
+            <a href="#" class="prev">
+                <i class="icon-left-open-big">
+                    <span class="sr-only">이전 페이지</span>
+                </i>
+            </a>
+            <a href="#" class="next">
+                <i class="icon-right-open-big">
+                    <span class="sr-only">다음 페이지</span>
+                </i>
+            </a>
+        </div>
+    </div>
+    
+    <!-- sub content start -->
+    <main id="content" class="order-info"><!-- 페이지명 클래스 -->
+        <div class="container">
+            <section class="mileage-order">
+                <h3>마일리지몰 주문현황</h3>
+                <article class="board-type-4">
+                    <ul>
+                        <li>
+                            <div class="list-header">
+                                <a href="#">
+                                    <figure>
+                                        <img src="/mobile/assets/images/sub/img_product01.png" alt="">
+                                    </figure>
+                                    <p class="item-title">
+                                        Botanical Therapy 스텝원 베이비치약 (무불소, 블루베리향) 
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="details-info">
+                                <dl>
+                                    <dt>수량</dt>
+                                    <dd>1</dd>
+                                    <dt>재고</dt>
+                                    <dd>있음</dd>
+                                    <dt>적립금</dt>
+                                    <dd>630</dd>
+                                    <dt>가격</dt>
+                                    <dd>71,900</dd>
+                                </dl>
+                                <div class="btn-area">
+                                    <a href="#" class="btn btn-xs btn-white" role="button">취소</a>
+                                    <a href="#" class="btn btn-xs btn-white" role="button">문의</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="list-header">
+                                <a href="#">
+                                    <figure>
+                                        <img src="/mobile/assets/images/sub/img_product01.png" alt="">
+                                    </figure>
+                                    <p class="item-title">
+                                        Botanical Therapy 스텝원 베이비치약 (무불소, 블루베리향) 
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="details-info">
+                                <dl>
+                                    <dt>수량</dt>
+                                    <dd>1</dd>
+                                    <dt>재고</dt>
+                                    <dd>있음</dd>
+                                    <dt>적립금</dt>
+                                    <dd>630</dd>
+                                    <dt>가격</dt>
+                                    <dd>71,900</dd>
+                                </dl>
+                                <div class="btn-area">
+                                    <a href="#" class="btn btn-xs btn-white" role="button">취소</a>
+                                    <a href="#" class="btn btn-xs btn-white" role="button">문의</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <nav class="paging" aria-label="Page navigation">
+                        <ul class="pagination">
+                            <li class="first">
+                                <a href="#" aria-label="Previous">
+                                    <i class="icon-angle-double-left"></i>
+                                </a>
+                            </li>
+                            <li class="prev">
+                                <a href="#" aria-label="Previous">
+                                    <i class="icon-angle-left"></i>
+                                </a>
+                            </li>
+                            <li class="active"><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">5</a></li>
+                            <li class="next">
+                                <a href="#" aria-label="Next">
+                                    <i class="icon-angle-right"></i>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#" aria-label="Next">
+                                    <i class="icon-angle-double-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </article>
+            </section>
+            <section class="big-deal">
+                <h3>앙주빅딜 주문현황</h3>
+                <article class="board-type-4">
+                    <ul>
+                        <li>
+                            <div class="list-header">
+                                <a href="#">
+                                    <figure>
+                                        <img src="/mobile/assets/images/sub/img_product01.png" alt="">
+                                    </figure>
+                                    <p class="item-title">
+                                        Botanical Therapy 스텝원 베이비치약 (무불소, 블루베리향) 
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="details-info">
+                                <dl>
+                                    <dt>수량</dt>
+                                    <dd>1</dd>
+                                    <dt>재고</dt>
+                                    <dd>있음</dd>
+                                    <dt>적립금</dt>
+                                    <dd>630</dd>
+                                    <dt>가격</dt>
+                                    <dd>71,900</dd>
+                                </dl>
+                                <div class="btn-area">
+                                    <a href="#" class="btn btn-xs btn-white" role="button">취소</a>
+                                    <a href="#" class="btn btn-xs btn-white" role="button">문의</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="list-header">
+                                <a href="#">
+                                    <figure>
+                                        <img src="/mobile/assets/images/sub/img_product01.png" alt="">
+                                    </figure>
+                                    <p class="item-title">
+                                        Botanical Therapy 스텝원 베이비치약 (무불소, 블루베리향) 
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="details-info">
+                                <dl>
+                                    <dt>수량</dt>
+                                    <dd>1</dd>
+                                    <dt>재고</dt>
+                                    <dd>있음</dd>
+                                    <dt>적립금</dt>
+                                    <dd>630</dd>
+                                    <dt>가격</dt>
+                                    <dd>71,900</dd>
+                                </dl>
+                                <div class="btn-area">
+                                    <a href="#" class="btn btn-xs btn-white" role="button">취소</a>
+                                    <a href="#" class="btn btn-xs btn-white" role="button">문의</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <nav class="paging" aria-label="Page navigation">
+                        <ul class="pagination">
+                            <li class="first">
+                                <a href="#" aria-label="Previous">
+                                    <i class="icon-angle-double-left"></i>
+                                </a>
+                            </li>
+                            <li class="prev">
+                                <a href="#" aria-label="Previous">
+                                    <i class="icon-angle-left"></i>
+                                </a>
+                            </li>
+                            <li class="active"><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">5</a></li>
+                            <li class="next">
+                                <a href="#" aria-label="Next">
+                                    <i class="icon-angle-right"></i>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#" aria-label="Next">
+                                    <i class="icon-angle-double-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </article>
+            </section> 
+        </div>
+    </main>
+    
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/mobile/inc/footer.php"); ?>
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/mobile/inc/docfoot.php"); ?>
+    <script src="/mobile/assets/js/common.js"></script>
+</body>
+</html>
