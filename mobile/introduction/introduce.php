721e50cc6a3728168f4873b8e2ce2bb529cb4286
Joahra <mk_ahra0522@gmail.com>
2018-01-13 16:33:50 +0900

    모바일 회사소개 완료

--- mobile/introduction/introduce.php
+++ mobile/introduction/introduce.php
@@ -75,8 +75,162 @@
                 </div>
                 <div class="section-content">
                     <article>
-                        <h5></h5>
+                        <div class="article-header">
+                            <h5>월간 &lt; ange앙쥬 &gt;</h5>
+                            <figure>
+                                <img src="/mobile/assets/images/introduction/img_magazine01.png" alt="">
+                            </figure>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" class="btn btn-md btn-pink" role="button">
+                                        <i class="icon-export"></i>앙쥬 미디어킷 보기
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="article-content">
+                            <h6>
+                                1998년에 창간한 월간 임신출산육아 전문 잡지입니다.
+                            </h6>
+                            <p class="info">
+                                임신, 출산, 육아에 대한 전문적이고 실용적인 정보를 고품격 기사와 사진으로 선보이고 있으며, 광고주와 독자 의견을 최대한 반영, 대한민국 육아 트렌드를 주도하고 있습니다. 무엇보다 최대 규모로 운영되는 무료 배포 시스템은 월간 &lt;ange앙쥬&gt;의 자랑입니다. 서울 수도권과 광역시의 대형 병원, 전문병원, 산후조리원은 물론 짐보리 가맹점과 백화점 매장 등에 이르기까지, 임산부와 아기 엄마가 방문하는 900여 곳의 장소에서 항상 만날 수 있습니다. 육아 잡지 최대 발행 부수와 함께 오랜 기간 열독률 조사에서 늘 1위를 차지한, 명실상부 우리나라 대표 육아 잡지 입니다.
+                            </p>
+                            <ul class="info-list">
+                                <li>
+                                    판형 5*7배판 변형(210×280mm)
+                                </li>
+                                <li>
+                                    지면 220~240쪽 내외
+                                </li>
+                                <li>
+                                    매월 6만 부 발행
+                                </li>
+                            </ul>
+                        </div>
                     </article>
+                    <article>
+                        <div class="article-header">
+                            <h5>계간 &lt;앙쥬백과&gt;</h5>
+                            <figure>
+                                <img src="/mobile/assets/images/introduction/img_magazine02.png" alt="">
+                            </figure>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" class="btn btn-md btn-pink" role="button">
+                                        <i class="icon-export"></i>앙쥬 미디어킷 보기
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="article-content">
+                            <h6>
+                               월간 &lt;ange앙쥬&gt;의 17년 노하우로 탄생한 임신출산육아백과입니다. 
+                            </h6>
+                            <p class="info">
+                                크고, 두껍고, 무거운 백과 스타일에서 탈피해, 엄마가 가방 속에 넣고	다니며 언제든 원할 때 펼쳐볼 수 있는 ‘손 안의 육아 바이블’입니다. 임신한 순간부터 육아에 한시름 놓게 되는 만 3세까지, 꼭 필요한 정보들을 망라했	으며, 1~3월, 4~6월, 7~9월, 10~12월 등 연 4회 발행을 통해 매호 정보를 업그레이드 하고 있습니다. 무크하우스의 독보적인 무료 배포 시스템을 통해 서울, 경기 및 6대 광역시의 20여 곳 산부인과에서 스페셜 기프트로 증정 중이며, 일반인들은 대형 서점에서 구입할 수 있습니다.
+                            </p>
+                            <ul class="info-list">
+                                <li>
+                                    판형 5*7 판 변형(165×220mm)
+                                </li>
+                                <li>
+                                    지면 300쪽 내외
+                                </li>
+                                <li>
+                                    분기별 3만 부 발행
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-header">
+                            <h5>온라인 사업</h5>
+                            <figure>
+                                <img src="/mobile/assets/images/introduction/img_online.png" alt="">
+                            </figure>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" class="btn btn-md btn-pink" role="button">
+                                        <i class="icon-export"></i>앙쥬 미디어킷 보기
+                                    </a>
+                                </p>
+                            </div> 
+                        </div>
+                        <div class="article-content">
+                            <h6>
+                               앙쥬 독자들의 커뮤니티 공간은 물론, 기업의 온라인 마케팅 플랫폼으로서 탁월한 효과를 자랑합니다.
+                            </h6>
+                            <p class="info">             
+                                &lt;ange앙쥬&gt;가 20여 년의 시장 변화 속에서도 굳건히 지금의 자리를 지킬 수 있었던 것은 마케팅의 중심이 온라인으로 이동하는 것에 신속히 대처했기 때문입니다.
+                                국내 육아 잡지 중 최초로 포털 사이트 www.ange.co.kr 운영을 시작했으며, 2017년 현재 가입자 수 56만여 명, 일 방문자 수 3만여 명, 매월 20, 30대 임산부 7,500여 명이 신규 가입을 할 만큼 타깃이 확실한 온라인 매체가 되었습니다. 앙쥬 독자들의 커뮤니티 공간은 물론, 기업의 온라인 마케팅 플랫폼으로서 탁월한 효과를 자랑합니다.
+                                앙쥬 홈페이지와는 별도로 모바일 웹, 네이버 앙쥬 블로그 및 파오인, 탭진 등의 애플리케이션과 다양한 경로의 SNS 활용 역시 온라인 사업의 일환으로, 온라인에서도 ‘빈틈없는’ 마케팅을 구사합니다. 최근 신개념 광고 채널, 앙쥬 광고센터를 오픈, 광고주가 직접 자사의 캠페인을 진행하고 실시간 모니터링을 하는 온라인 시스템 또한 구축했습니다. 이 모두가 광고주의 니즈(needs)와 급변하는 시장 상황에 대응하는 무크하우스의 전략입니다.
+                            </p>
+                            <ul class="info-list">
+                                <li>
+                                   56만여 명의 온라인 회원 확보
+                                </li>
+                                <li>
+                                    앙쥬 광고센터 운영
+                                </li>
+                                <li>
+                                    월간 &lt;ange앙쥬&gt;, 계간 &lt;앙쥬백과&gt; E-Book 발행
+                                </li>
+                                <li>
+                                    파오인, 탭진 등 애플리케이션
+                                </li>
+                                <li>
+                                    블로그, 카카오스토리, 페이스북 운영
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-header">
+                            <h5>앙쥬 스튜디오</h5>
+                            <figure>
+                                <img src="/mobile/assets/images/introduction/img_studio.png" alt="">
+                            </figure>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" class="btn btn-md btn-pink" role="button">
+                                        <i class="icon-export"></i>앙쥬스튜디오 미디어킷 보기
+                                    </a>
+                                </p>
+                            </div> 
+                        </div>
+                        <div class="article-content">
+                            <h6>
+                               &lt;ange앙쥬&gt;의 비주얼을 책임져 온 와인 스튜디오에서 기업 홍보를 위한 광고 사진과 디자인 토털 매니지먼트로 새롭게 출발합니다.
+                            </h6>
+                            <p class="info">             
+                               와인 스튜디오는 광고 사진 전문 스튜디오로서, 풍부한 경험과 감성, 노하우를 통해 광고주에게 최상의 비주얼 솔루션을 구현합니다. 더 넓고 쾌적해진 공간, 최신식 촬영 장비, 헤어・메이크업 아티스트와 그래픽 디자이너 상주 등으로 원 스톱 매니지먼트가 가능하며, 무엇보다 광고주 편의의 맞춤 환경을 제공합니다. 지면 광고, 브로슈어, 카탈로그 등 사진과 디자인이 최상의 조합을 이루는 비주얼로, 눈에 보이는 그 이상의 가치를 담아 광고주의 고민을 해결해드립니다.
+                            </p>
+                            <ul class="info-list">
+                                <li>광고 사진(카탈로그, 브로슈어, 리플렛, 웹페이지 등) 촬영</li>
+                                <li>CEO 프로필 및 개인 화보 및 프로필 촬영</li>
+                                <li>패션, 요리, 인테리어 등 촬영</li>
+                                <li>후보정 및 디자인까지 원 스톱 매니지먼트</li>
+                            </ul>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-header">
+                            <h5>앙쥬클럽</h5>
+                            <figure>
+                                <img src="/mobile/assets/images/introduction/img_ange_club.png" alt="">
+                            </figure>
+                        </div>
+                        <div class="article-content">
+                            <h6>
+                               초기 출산모에게 브랜드 인지도를 확고히 해주는 전령사
+                            </h6>
+                            <p class="info">             
+                                직접 찾아가는 서비스, 앙쥬클럽은 무크하우스와의 협업으로 오랜 기간 앙쥬의 발이 되어 왔습니다.
+                                앙쥬클럽의 멤버는 전문 간호사 출신으로 구성되었으며, 프리미엄 산후조리원에서 모유수유와 유방 관리에 대해 강의합니다. 또한 산모에게 &lt;ange앙쥬&gt;는 물론 임신출산육아 기업 및 브랜드를 직접 대면 홍보함으로써, 육아 초기부터 브랜드 인지도를 확고히 해주는 역할을 맡고 있습니다. 서비스가 제공되는 산후조리원 원장과 산모는 물론, 회원사인 광고주의 만족도가 단연 최고인 앙쥬만의 특별하고도 차별화된 서비스입니다.
+                            </p>
+                        </div>
+                    </article>
+                    
                 </div>
             </section>
         </div>
