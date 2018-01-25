2cb5ff55c8f3be9dbb7cb60b4fe94e73c01fad3d
Joahra <mk_ahra0522@gmail.com>
2018-01-25 19:43:55 +0900

    커뮤니티 추가 페이지 퍼블리싱

--- community/club_intro.php
+++ community/club_intro.php
@@ -24,9 +24,72 @@
                             </div>
                         </div>
                         <div class="section-content">
-                            <figure>
-                                <img src="/assets/images/community/intro_01.jpg" class="img-responsive" alt="">
-                            </figure>
+                            <p class="club-logo">
+                                <img src="/assets/images/community/img_club_logo.png" alt="">
+                            </p>
+                            <h3>
+                                사랑하는 만큼 아낌없이<br> 
+                                좋은 것만 해주고 싶은 엄마의 마음, 앙쥬클럽은<br> 
+                                <i class="blue">아기를 사랑하는 엄마들을 위한 육아 클럽</i>입니다.
+                            </h3>
+                            <p class="explain">
+                                임신 기간 동안 엄마의 몸은 전신에 걸쳐 많은 변화가 일어납니다.<br>
+                                아기를 낳은 후 엄마의 몸이 예전의 상태로 돌아가기까지는 최소 6주의 시간이 걸리며, <br>
+                                그 중 에서도 특히 최초 2주의 산후 조리기간은 매우 중요한 시기입니다.<br>
+                                바로 이 중요한 시기에 <i class="pink">앙쥬클럽에서 산모와 신생아 돌보기에 관한 교육을<br> 
+                                제공</i>해드립니다. <br><br>
+
+                                묘목을 잘 키우기 위해서는 뿌리 멀리서부터 조금씩 거름을 주면서 <br>
+                                나무가 잘 적응할 수 있도록 기다려야 하듯, 앙쥬클럽은 인내를 바탕으로 한 <br>
+                                지혜와 신뢰로 엄마들의 든든한 지지자와 후원자가 되겠습니다.
+                            </p>
+                            
+                            <article>
+                                <div class="title">
+                                    <h4>
+                                    주 고객층에게<br> 
+                                    <b>육아서비스 제공을 위한 육아클럽</b></h4>
+                                    <figure>
+                                        <img src="/assets/images/community/img_ange_club.png" alt="">
+                                    </figure>
+                                    <h4>
+                                        간호사 출신의<br>
+                                        전문가 구성·다양한 정보 제공
+                                    </h4>
+                                </div>
+                                <div class="info">
+                                    <ul>
+                                        <li>
+                                            <figure>
+                                                <img src="/assets/images/community/img_club01.png" alt="">
+                                            </figure>
+                                            <p class="figcaption"></p>
+                                        </li>
+                                        <li>
+                                            <figure>
+                                                <img src="" alt="">
+                                            </figure>
+                                            <p class="figcaption"></p>
+                                        </li>
+                                        <li>
+                                            <figure>
+                                                <img src="" alt="">
+                                            </figure>
+                                            <p class="figcaption"></p>
+                                        </li>
+                                        <li>
+                                            <figure>
+                                                <img src="" alt="">
+                                            </figure>
+                                            <p class="figcaption"></p>
+                                        </li>
+                                    </ul>
+                                    <ol>
+                                        <li></li>
+                                        <li></li>
+                                    </ol>
+                                </div>
+                            </article>
                         </div>
                     </section>
                 </div>
