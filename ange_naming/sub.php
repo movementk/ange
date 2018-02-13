c32b03977ea24d614f321ed87d2196561fbbfac2
kimyh5392 <mk.kimyh@gmail.com>
2018-02-13 14:39:31 +0900

    작명소 서브 가이드 퍼블리싱 작업중. . .

--- ange_naming/sub.php
+++ ange_naming/sub.php
@@ -80,6 +80,96 @@
                             </tbody>
                         </table>
                     </div>
+                    <br><br><br>
+                    <div class="table-wrap basket-table">
+                        <table class="table table-bordered">
+                            <colgroup>
+                                <col style="width: 75px;">
+                                <col style="width: 95px;">
+                                <col style="width: 45px;">
+                                <col style="width: 70px;">
+                                <col style="width: 40px;">
+                                <col style="width: 45px;">
+                                <col style="width: 70px;">
+                                <col style="width: 40px;">
+                                <col style="width: 45px;">
+                                <col style="width: 70px;">
+                                <col style="width: 40px;">
+                                <col style="width: 135px;">
+                                <col style="width: 70px;">
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th rowspan="2">음오행</th>
+                                    <th rowspan="2">이름</th>
+                                    <th colspan="3">성(姓)</th>
+                                    <th colspan="3">가운데이름자</th>
+                                    <th colspan="3">끝이름자</th>
+                                    <th rowspan="2">이름풀이</th>
+                                    <th rowspan="2">삭제</th>
+                                </tr>
+                                <tr>
+                                    <th>한자</th>
+                                    <th>훈과음</th>
+                                    <th>획수</th>
+                                    <th>한자</th>
+                                    <th>훈과음</th>
+                                    <th>획수</th>
+                                    <th>한자</th>
+                                    <th>훈과음</th>
+                                    <th>획수</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>金金金</td>
+                                    <td>신상철</td>
+                                    <td>申</td>
+                                    <td>납 신</td>
+                                    <td>5</td>
+                                    <td>尙</td>
+                                    <td>오히려 상</td>
+                                    <td>8</td>
+                                    <td>哲</td>
+                                    <td>밝을 철</td>
+                                    <td>10</td>
+                                    <td>
+                                        <div class="btn-explanation">
+                                            <a href="#" class="btn btn-xs btn-white" role="button">이름풀이</a>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="form-group">
+                                            <input type="checkbox">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>木木木</td>
+                                    <td>김경규</td>
+                                    <td>金</td>
+                                    <td>성 김</td>
+                                    <td>8</td>
+                                    <td>炅</td>
+                                    <td>빛날 경</td>
+                                    <td>8</td>
+                                    <td>奎</td>
+                                    <td>별 규</td>
+                                    <td>9</td>
+                                    <td>
+                                        <div class="btn-explanation">
+                                            <a href="#" class="btn btn-xs btn-white" role="button">이름풀이</a>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="form-group">
+                                            <input type="checkbox">
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
