77a14a5eef26142ff2e93b6a3159606748f1a0da
Joahra <mk_ahra0522@gmail.com>
2018-02-28 18:05:02 +0900

    앙쥬 팝업 수정1

--- ange_naming/real_time/result_popup.php
+++ ange_naming/real_time/result_popup.php
@@ -5,36 +5,135 @@
 <body id="default-popup">
     <section class="popup">
         <div class="section-header">
-            한자검색 
+            <h4>한자검색</h4>
         </div>
         <div class="section-content">
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content<br>
-            content
+            <div class="table-wrap">
+                <table class="table type-2">
+                    <thead>
+                        <tr>
+                            <th>한자</th>
+                            <th>훈</th>
+                            <th>음</th>
+                            <th>선택</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert01.png" alt="">
+                                </p>
+                            </td>
+                            <td>북방</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert02.png" alt="">
+                                </p>
+                            </td>
+                            <td>맡을</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert03.png" alt="">
+                                </p>
+                            </td>
+                            <td>아이밸</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert04.png" alt="">
+                                </p>
+                            </td>
+                            <td>수풀</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert05.png" alt="">
+                                </p>
+                            </td>
+                            <td>아이밸</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert06.png" alt="">
+                                </p>
+                            </td>
+                            <td>품팔이</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert07.png" alt="">
+                                </p>
+                            </td>
+                            <td>풍년들</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>
+                                    <img src="/assets/images/ange_naming/convert08.png" alt="">
+                                </p>
+                            </td>
+                            <td>임할</td>
+                            <td>임</td>
+                            <td>
+                                <div class="btn-confirm">
+                                    <a href="#" class="btn btn-sm" role="button">선택하기</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </section>
 </body>
